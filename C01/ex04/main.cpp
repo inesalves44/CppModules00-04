d194cf778169f4a09d7fbe7a88fa92e20196cca8
Ines Alves <idias-al@student.42porto.com>
2024-03-30 17:18:44 +0000

    42 - 30/3 17:18

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -1,24 +1,86 @@
-#include "Replace.h"
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
 
-void replaceString(char *argv[])
+std::string GetFileContents(std::string argv)
 {
-	std::string test = ".replace";
-	std:: string s3 = argv[1] + test;
+    std::string fileContents;
+    std::ifstream infile;
+    std::stringstream full;
+    
+    infile.open(argv.c_str(), std::ios::in);
+	if (!infile)
+	{
+		std::cerr << "The file cannot be open" << std::endl;
+		return NULL;
+	}
+    
+    full << infile.rdbuf();
+    fileContents = full.str();
+    return fileContents;
+    infile.close();
+}
 
-    Replace newClass = Replace(argv[1], argv[2], argv[3], s3);
-	newClass.GetFileContents();
-    newClass.ReplaceString();
+
+std::string SubstituteInString(std::string fileContents,std::string oldString, std::string newString)
+{
+    size_t begin;
+    std::string newfileContents;
+
+    begin = fileContents.find(oldString);
+    newfileContents = fileContents;
+    std::cout << newfileContents;
+
+    while (begin != std::string::npos)
+    {
+        newfileContents.erase(begin, (size_t)oldString.length());
+        newfileContents.insert(begin, newString);
+        begin = newfileContents.find(oldString);
+    }
+    
+    return newString;
+}
+
+void CreateAndWriteToNewFile(std::string argv, std::string fileContents,std::string oldString, std::string newString)
+{
+    std::string nameNewFile = argv + ".replace";
+    std::ofstream outfile;
+    std::string newFileContents;
+
+    if (argv == "" || oldString == "")
+    {
+        std::cerr << "the file or the old string cannot be empty" << std::endl;
+        return ;
+    }
+    outfile.open(nameNewFile.c_str(), std::ios::out);
+    if (!outfile)
+    {
+        std::cerr << "The file cannot be open" << std::endl;
+		return ;
+    }
+
+    newFileContents = SubstituteInString(fileContents, oldString, newString);
+
+    outfile << newFileContents;
+
+    outfile.close();
 }
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-	if (argc != 4)
-	{
-		std::cout << "The program needs 3 arguments" <<std::endl;
-		std::cout << "Correct usage: ./string originalFile stringToReplace newString" <<std::endl;
-		return 1;
-	}
-	replaceString(argv);
-	return 0;
+    std::string fileContents;
+
+    if (argc != 4)
+    {
+        std::cerr << "the program runs with: ./string <filename> OldString NewString" << std::endl;
+        return 1;
+    }
+
+    fileContents = GetFileContents(argv[1]);
+    CreateAndWriteToNewFile(argv[1], fileContents, argv[2], argv[3]);
+    
+    return 0;
 }
