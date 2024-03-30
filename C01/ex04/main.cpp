f3e468d4de6d6b5398fc2587b782b379095526d4
Inês Alves <inesalves@Iness-MBP.lan>
2024-03-30 00:16:08 +0000

    29/3 00:16

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -3,10 +3,18 @@
 int main(int argc, char *argv[])
 {
     (void)argc;
-    std::string test = ".replace";
+	std::string test = ".replace";
 	std:: string s3 = argv[1] + test;
+
+	if (argc != 4)
+	{
+		std::cout << "The program needs 3 arguments" <<std::endl;
+		std::cout << "Correct usage: ./string originalFile stringToReplace newString" <<std::endl;
+		return 1;
+	}
 
     Replace newClass = Replace(argv[1], argv[2], argv[3], s3);
 	newClass.GetFileContents();
     newClass.ReplaceString();
+	return 0;
 }
